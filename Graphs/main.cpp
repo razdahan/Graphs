068a39919decb61b64e66c276dd1b0eb33c2e92b
Raz dahan <razdahan12@gnail.com>
2020-05-02 22:27:55 +0300

    Finished prime basic.

--- Graphs/main.cpp
+++ Graphs/main.cpp
@@ -15,25 +15,26 @@ int main()
 	//declare the graph vertices
 	std::vector<vertex<int>*> vertices = { new vertex<int>(1), new vertex<int>(2), new vertex<int>(3) };
 	const std::vector<edge<int>*> edges;
-	graph<int> graph(vertices, edges);
+	graph<int> g(vertices, edges);
 
 	//add edges between graph vertices - adds them to the graph edges prop and the adj map
-	graph.add_edge(vertices[0], vertices[1], 4);
-	graph.add_edge(vertices[1], vertices[2], 6);
-	graph.add_edge(vertices[2], vertices[0], -12);
+	g.add_edge(vertices[0], vertices[1], 4);
+	g.add_edge(vertices[1], vertices[2], 6);
+	g.add_edge(vertices[2], vertices[0], -12);
 	
-	//
-	// dijkstra(&graph, vertices[0]);
-	// std::cout << "result dijkstra distance from " << vertices[0]->id << " to " << vertices[2]->id << " is ";
-	// std::cout << graph.d[vertices[2]] << std::endl;
-	//
-	// bfs(&graph, vertices[0]);
-	// std::cout << "result bfs distance from " << vertices[0]->id << " to " << vertices[2]->id << " is ";
-	// std::cout << graph.d[vertices[2]] << std::endl;
-	//
-	// std::cout << "bellman ford result " << bellman_ford(&graph, vertices[0]) << std::endl;
+	
+	 dijkstra(&g, vertices[0]);
+	 std::cout << "result dijkstra distance from " << vertices[0]->id << " to " << vertices[2]->id << " is ";
+	 std::cout << g.d[vertices[2]] << std::endl;
+	
+	 bfs(&g, vertices[0]);
+	 std::cout << "result bfs distance from " << vertices[0]->id << " to " << vertices[2]->id << " is ";
+	 std::cout << g.d[vertices[2]] << std::endl;
+	
+	 std::cout << "bellman ford result " << bellman_ford(&g, vertices[0]) << std::endl;
 
-	prime_basic(&graph);
+	graph<int> *graph =prime_basic(&g);
+	std::cout << graph->edges.size()<< std::endl;
 
 	std::string hold;
 	std::cin >> hold;
--- Graphs/primebasic.h
+++ Graphs/primebasic.h
@@ -3,28 +3,90 @@
 #include "vertex.h"
 #include "graph.h"
 #include "set"
-
+#include <algorithm>
 
 
 template<class T>
-void prime_basic(graph<T> *graph)
+graph<T>* prime_basic(graph<T> *g)
 {
 
-	std::vector<vertex<T>*> v1={graph->vertices[0]};
-	std::vector<vertex<T>*> v2=graph->vertices;
+	std::vector<vertex<T>*> v1={g->vertices[0]};
+	std::vector<vertex<T>*> v2=g->vertices;
 	v2.erase(remove_if(begin(v2), end(v2),
 		[&](auto x) {return find(begin(v1), end(v1), x) != end(v1); }), end(v2));
 	
 	std::vector<edge<T>*> e1;
 	auto index = 1;
+	char end;
 	do
 	{
-	/*Implement the algorithm*/
+		auto e12 = get_cross_group(g, v1,v2);
+		edge<T>* e_min = min_edge(e12);
 		
-	} while (v1 != graph->vertices);
+		/*remove by value because the value represent the vertex location in the memory so its like a key*/
+		if (exist(e_min->src, v1)) {
+			v1.push_back(e_min->dest);
+			v2.erase(std::remove(v2.begin(), v2.end(), e_min->dest), v2.end());
+		}
+		else
+		{
+			v1.push_back(e_min->src);
+			v2.erase(std::remove(v2.begin(), v2.end(), e_min->src), v2.end());
+		}
+			
+		e1.push_back(e_min);
+        /*sorting so I can compare the 2 vectors without the need to worry about elements locations.*/
+		std::sort(v1.begin(), v1.end());
+		std::sort(g->vertices.begin(), g->vertices.end());
+		
+	} while (v1 != g->vertices);
 
-	std::cout << std::endl;
-	std::cout << graph->vertices.size() << std::endl;
-	std::cout << v2.size() << std::endl;
 	
+	return new graph<T>(v1, e1);
+	
+}
+
+
+template<class T>
+std::vector<edge<T>*> get_cross_group(graph<T>* graph,std::vector<vertex<T>*> v1, std::vector<vertex<T>*> v2)
+{
+	std::vector<edge<T>*> result;;
+	for(edge<T>* u:graph->edges)
+	{
+		
+		if(exist(u->src,v1)&&exist(u->dest,v2)|| exist(u->src, v2) && exist(u->dest, v1))
+		{
+			std::cout << "pushing "<<u <<"to the cross group" << std::endl;
+			result.push_back(u);
+		}
+	}
+	return result;
+}
+
+
+template<class T>
+bool exist(vertex<T> *v, std::vector<vertex<T>*> vertex_list)
+{
+	for(vertex<T> *u:vertex_list)
+	{
+		if (u == v) {
+			return true;
+		}
+	}
+	return false;
+}
+
+template<class T>
+edge<T>* min_edge(std::vector<edge<T>*> edges)
+{
+	
+	edge<T>* min_edge=edges.back();
+	for(edge<T>* e:edges)
+	{
+		if(min_edge->weight>e->weight)
+		{
+			min_edge = e;
+		}
+	}
+	return min_edge;
 }
